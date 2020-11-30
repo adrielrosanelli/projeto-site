80f391597056adcdafdd8b5ff4e09524ecb17cfb
adriel-senac <sdadasd@sdasda.com>
2020-11-30 19:58:18 -0300

    Mudanças no layout

--- application/views/login.php
+++ application/views/login.php
@@ -3,24 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Realize O Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h2>Realize O Login</h2>
+    
 <div class="container">
-    <div class="row">
-        <div class="col">
-            
-        </div>
-    </div>
-</div>
-
-<?php echo validation_errors();?>
+    <div class="row" style="height: 1000px;">
+        <div class="col-12">
+        <div class="card" style="text-align:center; justify-content: center; display:flex;height: 400px;">
+  <div class="card-body">
+    <h2 class="card-title">Realize o Login</h2>
+    <?php echo validation_errors();?>
 <?=!empty($this->session->userdata('mensagem'))?$this->session->userdata('mensagem'):null;?>
 <form method="post" action="<?=base_url('login/action')?>">
     E-mail: <input type="email" name="email"><br>
     senha: <input type="password" name="senha"><br>
     <input type="submit" value="Entrar">
 </form>
+    <a href="#" class="btn btn-primary">Cadastrar-se</a>
+  </div>
+</div>
+
+
+
+
+        
+        </div>
+    </div>
+</div>
+
+
+
+
+
 </body>
 </html>
--- application/views/profissional/form.php
+++ application/views/profissional/form.php
@@ -4,10 +4,17 @@
 <form enctype="multipart/form-data" method="POST" action="<?=$action?>">
 <input type="text" name="nome" value="<?=$nome?>" placeholder="Digite seu nome"><br>
 <input type="email" name="email" value="<?=$email?>" placeholder="Digite seu e-mail"><br>
-<input type="text" name="descricao" value="<?=$descricao?>" placeholder="Descreva-se"><br>
+<textarea name="descricao" id="descricao" cols="30" rows="10"><?=$descricao?></textarea><br>
 <input type="date" name="dataNascimento" value="<?=$dataNascimento?>" placeholder="Digite a sua data de Nascimento"><br>
 <input type="number" name="escolaridade" value="<?=$escolaridade?>" placeholder="Digite sua escolaridade"><br>
 <input type="number" name="precoHora" value="<?=$precoHora?>" placeholder="Digite o preço da sua Hora"><br>
+<select name="status" id="status">
+    <option value="<?=$status?>">Selecione o seu</option>
+    <option value="<?=$status?>">Selecione o seu</option>
+    <option value="<?=$status?>">Selecione o seu</option>
+    <option value="<?=$status?>">Selecione o seu</option>
+    <option value="<?=$status?>">Selecione o seu</option>
+</select>
 <input type="text" name="status" value="<?=$status?>" placeholder="Digite sua disponibilidade"><br>
 <input name="arquivo" type="file"><br>
 <?php
@@ -19,3 +26,6 @@ if(!empty($arquivo)){
 <input type="submit" value="Gravar">
 </form>
 </fieldset> 
+
+
+<!-- <input type="text" name="descricao" value="<?=$descricao?>" placeholder="Descreva-se"><br> -->
